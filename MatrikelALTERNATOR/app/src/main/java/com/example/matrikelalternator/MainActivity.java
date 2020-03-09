da9c515cc134c1414e17cdb3381f94a9a884aa9b
Lukas Kofler <lukofler@edu.aau.at>
2020-03-09 18:50:01 +0100

    Textfields and Buttons added

--- MatrikelALTERNATOR/app/src/main/java/com/example/matrikelalternator/MainActivity.java
+++ MatrikelALTERNATOR/app/src/main/java/com/example/matrikelalternator/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.matrikelalternator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText Matrikelnummer;
+    private TextView EnterMNr;
+    private Button Abschicken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Assigns to ID etMNr in XML
+        Matrikelnummer = findViewById(R.id.etMNr);
+        EnterMNr = findViewById(R.id.tvMNR);
+        Abschicken = findViewById(R.id.btnAbschicken);
     }
 }
