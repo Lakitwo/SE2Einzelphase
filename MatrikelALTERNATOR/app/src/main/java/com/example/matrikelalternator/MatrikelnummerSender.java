fcfef561e189937ca41b1bed0aca45f8f2231ee9
Lukas Kofler <lukofler@edu.aau.at>
2020-03-12 13:21:59 +0100

    TRY handling sending MNr to Server and recieving Answer in MatrikelnummerSender.class

--- MatrikelALTERNATOR/app/src/main/java/com/example/matrikelalternator/MatrikelnummerSender.java
+++ MatrikelALTERNATOR/app/src/main/java/com/example/matrikelalternator/MatrikelnummerSender.java
@@ -1,35 +1,48 @@
 package com.example.matrikelalternator;
 
 import android.os.AsyncTask;
+import android.view.contentcapture.DataRemovalRequest;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.net.SocketException;
 
-public class MatrikelnummerSender extends AsyncTask<String, Void, Void> {
+public class MatrikelnummerSender extends AsyncTask<String, Void, String> {
 
     Socket s;
     DataOutputStream dos;
-    PrintWriter pw;
+    BufferedReader bufferedReader;
+    String sentMatrikelnummer;
+    String recievedMessage;
 
     @Override
-    protected Void doInBackground(String... voids) {
+    protected String doInBackground(String... voids) {
 
-        String recievedMatrikelnummer = voids[0];
+
 
         try {
             s = new Socket("se2-isys.aau.at", 53212);
-            pw = new PrintWriter(s.getOutputStream());
-            pw.write(recievedMatrikelnummer);
-            pw.flush();
-            pw.close();
+            if(!s.isConnected())
+                throw new SocketException("Socket not connected!");
+            dos = new DataOutputStream(s.getOutputStream());
+            bufferedReader = new BufferedReader(new InputStreamReader(s.getInputStream()));
+
+            //sending Data to Server
+            dos.writeBytes(sentMatrikelnummer + '\n');
+
+            //reading Data from Server
+            recievedMessage = bufferedReader.readLine();
+
+            //closing Socket
             s.close();
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        return null;
+        return recievedMessage;
     }
 }
