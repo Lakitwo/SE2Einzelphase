6653595c301f7d00a06fd73c90032a5da6600782
Lukas Kofler <lukofler@edu.aau.at>
2020-03-13 11:46:27 +0100

    replaced function of send-Method with OnClickListener implemented AsyncTask to get Answer from Server after accessing the Server, not while accessing it accesses of Variables declarated in MatrikelnummerSender set to private

--- MatrikelALTERNATOR/app/src/main/java/com/example/matrikelalternator/MainActivity.java
+++ MatrikelALTERNATOR/app/src/main/java/com/example/matrikelalternator/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.matrikelalternator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView EnterMNr;
     private TextView ServerAntwort;
     private Button Abschicken;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +28,23 @@ public class MainActivity extends AppCompatActivity {
         EnterMNr = findViewById(R.id.tvMNR);
         Abschicken = findViewById(R.id.btnAbschicken);
         ServerAntwort = findViewById(R.id.servResp);
+
+        Abschicken.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MatrikelnummerSender ms = new MatrikelnummerSender(Matrikelnummer.getText().toString());
+                AsyncTask asyncTask = ms.execute();
+                try {
+                    asyncTask.get();
+                } catch (Exception e) {
+                    System.out.println("Geht nix!!");
+                }
+                ServerAntwort.setText((String) ms.getRecievedMessage());
+            }
+        });
+
+
     }
 
-    public void send(View view){
-        MatrikelnummerSender ms = new MatrikelnummerSender(Matrikelnummer.getText().toString());
-        ms.execute(Matrikelnummer.getText().toString());
-        ServerAntwort.setText(ms.getRecievedMessage());
-    }
+
 }
--- MatrikelALTERNATOR/app/src/main/java/com/example/matrikelalternator/MatrikelnummerSender.java
+++ MatrikelALTERNATOR/app/src/main/java/com/example/matrikelalternator/MatrikelnummerSender.java
@@ -1,49 +1,49 @@
 package com.example.matrikelalternator;
 
 import android.os.AsyncTask;
-import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.SocketException;
 
-public class MatrikelnummerSender extends AsyncTask<String, Integer, String> {
+public class MatrikelnummerSender extends AsyncTask<Object, Object, Object> {
 
-    Socket s;
-    DataOutputStream dos;
-    BufferedReader bufferedReader;
-    String sentMatrikelnummer;
-    String recievedMessage;
-    //PrintWriter printWriter;
+    private Socket s;
+    private DataOutputStream dos;
+    private BufferedReader bufferedReader;
+    private String sentMatrikelnummer;
+    private String recievedMessage;
 
-    public MatrikelnummerSender(String sentMatrikelnummer) {
-        this.sentMatrikelnummer = sentMatrikelnummer;
+
+    MatrikelnummerSender(Object sentMatrikelnummer) {
+        this.sentMatrikelnummer = sentMatrikelnummer.toString();
     }
 
     @Override
-    protected String doInBackground(String[] strings) {
-
-
+    protected Object doInBackground(Object... params) {
 
         try {
             s = new Socket("se2-isys.aau.at", 53212);
-            if(!s.isConnected())
+            if (!s.isConnected())
                 throw new SocketException("Socket not connected!");
 
             dos = new DataOutputStream(s.getOutputStream());
             bufferedReader = new BufferedReader(new InputStreamReader(s.getInputStream()));
 
             //sending Data to Server
-            dos.writeBytes(sentMatrikelnummer + '\n');
+            try {
+                dos.writeBytes(sentMatrikelnummer + '\n');
+                // wait(50000);
+            } catch (Exception e) {
+                System.out.println("Failed to send MNr to Server");
+            }
 
             //reading Data from Server
             recievedMessage = bufferedReader.readLine();
 
-            //printWriter.write(recievedMessage);
 
             //closing Socket
             s.close();
@@ -55,7 +55,8 @@ public class MatrikelnummerSender extends AsyncTask<String, Integer, String> {
         return recievedMessage;
     }
 
-    public String getRecievedMessage(){
+    String getRecievedMessage() {
         return this.recievedMessage;
     }
+
 }
